0072b51fd7887b42bc5ed1def9856fd31703fed7
Jesús Antonio Flores González <jesusfloresglz12@outlook.com>
2026-02-22 01:23:15 -0600

    Agregada la posibilidad de cambiar el puerto del servidor de frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  // Carga las variables de entorno del archivo .env
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    server: {
+      port: parseInt(env.VITE_PORT) || 3000, // Lee el puerto del .env o usa 3000 por defecto
+    }
+  }
 })
