a4e566ade0814a7c1a7f6eb383848bba90c99e2b
Jesús Antonio Flores González <jesusfloresglz12@outlook.com>
2026-02-22 02:24:23 -0600

    Arreglos para el host

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,13 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  // Carga las variables de entorno del archivo .env
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
     plugins: [react()],
     server: {
+      host: env.VITE_HOST || true,
       port: parseInt(env.VITE_PORT) || 3006
     }
   }
