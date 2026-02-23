2aef79d83f32cd597650195f0399d259868a431e
Jesus Antonio Flores Gonzalez <jesusfloresglz12@outlook.com>
2026-02-23 12:50:12 -0600

    Bugfix de desconexiones inesperadas de la base de datos

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,6 +6,7 @@ const dbConfig = {
     port: process.env.DB_PORT || 3306,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
@@ -15,8 +16,15 @@ const pool = mysql.createPool(dbConfig);
 
 const initializeDatabase = async () => {
     try {
-        await pool.query(`CREATE DATABASE IF NOT EXISTS \`${process.env.DB_NAME}\``);
-        await pool.query(`USE \`${process.env.DB_NAME}\``);
+        const tempConn = await mysql.createConnection({
+            host: process.env.DB_HOST,
+            port: process.env.DB_PORT || 3306,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+        });
+        await tempConn.query(`CREATE DATABASE IF NOT EXISTS \`${process.env.DB_NAME}\``);
+        await tempConn.end();
+        
         return pool;
     } catch (error) {
         console.error('Error al inicializar la base de datos:', error.message);
